20cb20a025568a61ddc8567dc61ec689a0ae98a3
seungho0904 <61607485+seungho0904@users.noreply.github.com>
2023-09-04 15:53:40 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,5 +6,9 @@ module.exports = {
     images: {
       domains: ['media.licdn.com'], // Add 'media.licdn.com' to the list of domains
     },
+  output: {
+    // Set the export target to static
+    // This ensures that the app is exported as static HTML files
+    target: 'static',
     // Other Next.js configuration options...
-  };
+  }; 
