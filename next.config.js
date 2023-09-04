f57d333549b3fe12e6a9a33b8cecc5523d6c740e
seungho0904 <61607485+seungho0904@users.noreply.github.com>
2023-09-04 16:12:52 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {  
+  trailingSlash: true, 
+  images: {
+    domains: ['media.licdn.com'], // Add 'media.licdn.com' to the list of domains
+  },
   output: "export",
 };
-  
+
 module.exports = nextConfig;
